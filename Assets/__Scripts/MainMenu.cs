c7b5d83f8e943ccc8218bbda5a0c61b0d0734e7c
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-01-25 13:55:42 -0600

    Added Two Scenes

---
+++ Assets/__Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void SinglePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+
+    
+}
---
+++ Assets/__Scripts/SinglePlayerManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SinglePlayerManager : MonoBehaviour
+{
+    public void goBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
