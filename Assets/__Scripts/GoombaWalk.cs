9664d433d52f8cd5a5a590f3986a2130b400dcfe
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-01-26 13:45:12 -0600

    The goomba walks now

--- Assets/__Scripts/GoombaWalk.cs
+++ Assets/__Scripts/GoombaWalk.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class GoombaWalk : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 4f;      // The speed in m/s
+    public int dir = 1;
+    private Animator animator;
+
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    // This is a Property: A method that acts like a field
+    public Vector3 pos
+    {                                                     // a
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
     void Update()
     {
-        
+        Move();
+        //if (pos.x > Screen.width - 40 || pos.x < 40)
+        //{                      // a
+        //    // We're off the bottom, so destroy this GameObject            // b
+        //    this.gameObject.transform.Rotate(0, 180, 0);
+        //    this.dir *= -1;// b
+        //}
+        if (pos.x > 25F || pos.x < -20)
+        {
+            //Vector3 tempPos = pos;
+            //tempPos.x = -25;
+            //pos = tempPos;
+            this.gameObject.transform.Rotate(0, 180, 0);
+            this.dir *= -1;// b
+        }
+    }
+    public virtual void Move()
+    {                                             // b
+        Vector3 tempPos = pos;
+        tempPos.x += dir * speed * Time.deltaTime;
+        pos = tempPos;
     }
 }
