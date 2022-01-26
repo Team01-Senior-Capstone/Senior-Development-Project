f0e96a48611fe6ad6d06acde0409b5ffa1b859c2
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-01-26 14:10:45 -0600

    Worked on Multiplayer Scene

--- Assets/__Scripts/MultiPlayerManager.cs
+++ Assets/__Scripts/MultiPlayerManager.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 using UnityEngine.SceneManagement;
 
 public class MultiPlayerManager : MonoBehaviour
 {
+    public Button hostButton;
+    public Button joinButton;
+    public TMP_InputField roomName;
+
+    public void Awake()
+    {
+        roomName.gameObject.SetActive(false);
+    }
+
+    public void host()
+    {
+        hostButton.gameObject.SetActive(false);
+        joinButton.gameObject.SetActive(false);
+        roomName.gameObject.SetActive(true);
+    }
+
     public void goBack()
     {
         SceneManager.LoadScene("Main Menu");
