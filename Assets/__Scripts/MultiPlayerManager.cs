faccac5cb5154533723075150a702b049adee952
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-01-26 22:20:15 -0600

    Worked on Autofill

--- Assets/__Scripts/MultiPlayerManager.cs
+++ Assets/__Scripts/MultiPlayerManager.cs
@@ -5,12 +5,25 @@ using UnityEngine.UI;
 using TMPro;
 
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class MultiPlayerManager : MonoBehaviour
 {
     public Button hostButton;
     public Button joinButton;
+    public Button submit;
     public TMP_InputField roomName;
+
+    private static System.Random random = new System.Random();
+
+    public int roomNameLength = 7;
+
+    public string getRandomString()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, roomNameLength)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
 
     public void Awake()
     {
@@ -22,10 +35,17 @@ public class MultiPlayerManager : MonoBehaviour
         hostButton.gameObject.SetActive(false);
         joinButton.gameObject.SetActive(false);
         roomName.gameObject.SetActive(true);
+        roomName.placeholder.GetComponent<Text>().text = getRandomString();
+        submit.gameObject.SetActive(true);
     }
 
     public void goBack()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void onSumbit()
+    {
+        Debug.Log(roomName.text);
     }
 }
