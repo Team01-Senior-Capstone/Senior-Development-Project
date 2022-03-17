0c81117274c0f96849b0321c649ac12beeaa8c25
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-03-17 15:24:45 -0500

    perhaps

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class InstructionsManager : MonoBehaviour
 {
+    public GameObject[] panels;
+    int curIndex = 0;
     // Start is called before the first frame update
     public void goBack()
     {
         SceneManager.LoadScene("Main Menu");
     }
+
+    public void goForward()
+    {
+        //Delete the old panel
+        //increase the index
+        //Instantiate panels[curIndex]
+    }
+
 }
