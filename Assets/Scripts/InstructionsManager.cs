218b14f0b58170c371dff4c783d0f848c248f314
gblodgett <35748044+GBlodgett35@users.noreply.github.com>
2022-03-17 22:29:32 -0500

    Bug fixed, made buttons disable when you can't go farther

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InstructionsManager : MonoBehaviour
 {
     public GameObject[] Panels;
     GameObject curSlide;
+    public Button backButton, forwardButton;
     int curIndex = 0;
     public GameObject anchor;
+
+    int numSlides = 3;
     
     // Start is called before the first frame update
     public void Start()
     {
+        Debug.Log("Start called");
         curSlide = Instantiate(Panels[curIndex], anchor.transform.position, Quaternion.identity);
+        curSlide.transform.SetParent(anchor.transform);
+        backButton.interactable = false;
     }
 
     public void goBack()
@@ -23,10 +30,15 @@ public class InstructionsManager : MonoBehaviour
 
     public void slideForward()
     {
-        if (curIndex < 3)
+        if (curIndex < numSlides)
         {
             Destroy(curSlide);
             curIndex++;
+            backButton.interactable = true;
+            if(curIndex == numSlides)
+            {
+                forwardButton.interactable = false;
+            }
             curSlide = Instantiate(Panels[curIndex], anchor.transform.position, Quaternion.identity);
             curSlide.transform.SetParent(anchor.transform);
         }
@@ -34,16 +46,18 @@ public class InstructionsManager : MonoBehaviour
 
     public void slideBackward()
     {
-        Debug.Log("entered function");
 
         if (curIndex > 0)
         {
             Destroy(curSlide);
             curIndex--;
+            forwardButton.interactable = true;
+            if(curIndex == 0)
+            {
+                backButton.interactable = false;
+            }
             curSlide = Instantiate(Panels[curIndex], anchor.transform.position, Quaternion.identity);
             curSlide.transform.SetParent(anchor.transform);
         }
-
-        Debug.Log("exited function");
     }
 }
