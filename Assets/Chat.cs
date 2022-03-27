732aae448d4740f81a66a0729930eef0c1f4f7ba
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-03-27 17:08:03 -0500

    bug fixes

--- Assets/Chat.cs
+++ Assets/Chat.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Chat : MonoBehaviour
 {
@@ -30,22 +31,28 @@ public class Chat : MonoBehaviour
     public IEnumerator fadeChat()
     {
         yield return new WaitForSeconds(2f);
-        float startValue = scroll.GetComponent<Renderer>().material.color.a;
+        float startValue = scroll.GetComponent<Image>().color.a;
         float time = 0;
         float duration = 2f;
         while (time < duration)
         {
-            //Color newColor = r.color;
-            //Color barColor = loadingBar.GetComponent<MeshRenderer>().material.color;
-            Color a = scroll.GetComponent<Material>().color;
-            a.a = Mathf.Lerp(startValue, 1, time / duration);
-            ///barColor.a = newColor.a;
-            //loadingBar.GetComponent<MeshRenderer>().material.color = barColor;
-            //r.color = newColor;
-            scroll.GetComponent<Renderer>().material.color = a;
+           
+            Color a = scroll.GetComponent<Image>().color;
+            a.a = Mathf.Lerp(startValue, 0, time / duration);
+            scroll.GetComponent<Image>().color = a;
+
+            TMP_Text[] children = scroll.GetComponentsInChildren<TMP_Text>();
+            Color newColor;
+            foreach (TMP_Text child in children)
+            {
+                newColor = child.color;
+                newColor.a = a.a;
+                child.color = newColor;
+            }
             time += Time.deltaTime;
             yield return null;
         }
+        //scroll.SetActive(false);
     }
     public void sendChat()
     {
@@ -55,9 +62,10 @@ public class Chat : MonoBehaviour
         {
             textToSend = textToSend.Substring(0, MAX_CHAT_SIZE);
         }
-        addChat(textToSend);
+        addChat("Me: " + textToSend);
         ns.SendChatMessage(textToSend);
         StartCoroutine(fadeChat());
+        txtInput.text = "";
     }
 
     public IEnumerator checkForChat()
@@ -76,14 +84,47 @@ public class Chat : MonoBehaviour
 
     void addChat(string chatMessage)
     {
+        Debug.Log("Adding chat with text: " + chatMessage);
         GameObject newC = Instantiate(chatMessagePrefab);
-        newC.transform.SetParent(content.transform);
+        newC.transform.SetParent(content.transform, false);
         newC.GetComponent<TMP_Text>().text = chatMessage;
+    }
+
+    public void restoreChat()
+    {
+        Color newC = scroll.GetComponent<Image>().color;
+        newC.a = 100.0f / 255.0f;
+        scroll.GetComponent<Image>().color = newC;
+        scroll.SetActive(true);
+    }
+
+    public void minimizeChat() 
+    {
+        Color a = scroll.GetComponent<Image>().color;
+        a.a = 0;
+        scroll.GetComponent<Image>().color = a;
+
+        TMP_Text[] children = scroll.GetComponentsInChildren<TMP_Text>();
+        Color newColor;
+        foreach (TMP_Text child in children)
+        {
+            newColor = child.color;
+            newColor.a = a.a;
+            child.color = newColor;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            sendChat();
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            sendChat();
+        }
     }
 }
