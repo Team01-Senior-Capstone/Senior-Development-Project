a41038f7fb18ae224d8a7c6e50c4e0cc4735964a
GBlodgett35 <35748044+GBlodgett35@users.noreply.github.com>
2022-04-05 18:07:31 -0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -29,32 +29,53 @@ public class MainMenu : MonoBehaviour
 
     public void Update()
     {
-        StartCoroutine(checkInternetConnection((isConnected) =>
+        try
         {
-            if (!isConnected)
+            StartCoroutine(checkInternetConnection((isConnected) =>
             {
-                multiplayerButton.interactable = false;
-                offlineSymbol.SetActive(true);
-            }
-            else
-            {
-                multiplayerButton.interactable = true;
-                offlineSymbol.SetActive(false);
-            }
-        }));
-    }
-    public static IEnumerator checkInternetConnection(Action<bool> syncResult)
-    {
-        const string echoServer = "https://www.harding.edu/";
-
-        bool result;
-        using (var request = UnityWebRequest.Head(echoServer))
+                if (!isConnected)
+                {
+                    multiplayerButton.interactable = false;
+                    offlineSymbol.SetActive(true);
+                }
+                else
+                {
+                    multiplayerButton.interactable = true;
+                    offlineSymbol.SetActive(false);
+                }
+            }));
+        } 
+        catch
         {
-            request.timeout = 5;
-            yield return request.SendWebRequest();
-            result = !request.isNetworkError && !request.isHttpError && request.responseCode == 200;
+            multiplayerButton.interactable = false;
+            offlineSymbol.SetActive(true);
         }
-        syncResult(result);
+    }
+    //public static IEnumerator checkInternetConnection(Action<bool> syncResult)
+    //{
+    //    const string echoServer = "https://www.harding.edu/";
+
+    //    bool result;
+    //    using (var request = UnityWebRequest.Head(echoServer))
+    //    {
+    //        request.timeout = 5;
+    //        yield return request.SendWebRequest();
+    //        result = !request.isNetworkError && !request.isHttpError && request.responseCode == 200;
+    //    }
+    //    syncResult(result);
+    //}
+    IEnumerator checkInternetConnection(Action<bool> action)
+    {
+        UnityWebRequest request = new UnityWebRequest("https://www.harding.edu/");
+        yield return request.SendWebRequest();
+        if (request.error != null)
+        {
+            action(false);
+        }
+        else
+        {
+            action(true);
+        }
     }
 
     //IEnumerator checkInternetConnection(Action<bool> action)
